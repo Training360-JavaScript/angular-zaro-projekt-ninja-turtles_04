9b96719f09b4a11369016c64ac54030d846e40ca
Kocsis Gábor <ifjkocsis.gabor@gmail.com>
2022-02-09 20:18:18 +0100

    bill, categorry, customer, order query ready

--- ninjacars/src/app/page/edit-category/edit-category.component.ts
+++ ninjacars/src/app/page/edit-category/edit-category.component.ts
@@ -1,4 +1,8 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from 'src/app/model/category';
+import { CategoryService } from 'src/app/service/category.service';
 
 @Component({
   selector: 'app-edit-category',
@@ -6,10 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-category.component.scss']
 })
 export class EditCategoryComponent implements OnInit {
+  category$!: Observable<Category>
 
-  constructor() { }
+  constructor(private categoryService: CategoryService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe({
+      next: param => this.category$ = this.categoryService.getOne(param['id'])
+    })
   }
 
 }
--- ninjacars/src/app/page/edit-customer/edit-customer.component.ts
+++ ninjacars/src/app/page/edit-customer/edit-customer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Customer } from 'src/app/model/customer';
+import { CustomerService } from 'src/app/service/customer.service';
 
 @Component({
   selector: 'app-edit-customer',
@@ -7,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditCustomerComponent implements OnInit {
 
-  constructor() { }
+  customer$!: Observable<Customer>
+
+  constructor(private customerService: CustomerService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe({
+      next: param => this.customer$ = this.customerService.getOne(param['id'])
+    })
   }
 
 }
