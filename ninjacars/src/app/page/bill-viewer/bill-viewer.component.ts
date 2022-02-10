a7c31f47a21cd2de229dce99806b76b80bdf8836
Porkoláb Ádám <adam.porkolab@gmail.com>
2022-02-10 07:22:32 +0100

    All viewers are done.

--- ninjacars/src/app/page/bill-viewer/bill-viewer.component.ts
+++ ninjacars/src/app/page/bill-viewer/bill-viewer.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Bill } from './../../model/bill';
+import { BillService } from './../../service/bill.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-bill-viewer',
   templateUrl: './bill-viewer.component.html',
-  styleUrls: ['./bill-viewer.component.scss']
+  styleUrls: ['./bill-viewer.component.scss'],
 })
 export class BillViewerComponent implements OnInit {
+  @Input() bills: Bill[] = [];
+  bills$: Observable<Bill[]> = this.billService.getAll();
 
-  constructor() { }
+  keys: string[] = Object.keys(new Bill());
+  phrase: string = '';
+  filterKey: string = '';
 
-  ngOnInit(): void {
+  direction: string = 'asc';
+  column: string = 'id';
+  type: string | number = 'number';
+
+  setSortParams(direction: string, column: string, type: string) {
+    this.direction = direction;
+    let key =
+      this.keys.find((key) => key.toLowerCase() === column.toLowerCase()) ||
+      'id';
+    this.column = key;
+    // this.type = typeof Bill[key];
   }
+  constructor(private billService: BillService) {}
 
+  ngOnInit(): void {}
 }
