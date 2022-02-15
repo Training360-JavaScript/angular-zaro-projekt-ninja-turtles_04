73e06a928df44a74b04523955e5a3c23f31d3b50
klvinbagoly <kbalazsk1@gmail.com>
2022-02-15 08:37:12 +0100

    Second chart

--- ninjacars/src/app/charts/pie-chart/pie-chart.component.ts
+++ ninjacars/src/app/charts/pie-chart/pie-chart.component.ts
@@ -17,7 +17,7 @@ export class PieChartComponent implements OnInit {
 
   @Input() chartLabels: Array<any> = ['Red', 'Green', 'Yellow', 'Grey', 'Dark Grey'];
 
-  public chartColors: Array<any> = [
+  @Input() chartColors: Array<any> = [
     {
       backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'],
       hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5', '#616774'],
